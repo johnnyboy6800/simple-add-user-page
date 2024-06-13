5143ffb4a18f3b958e3d24020b843d2f2042a037
João Lucas <152038853+johnnyboy6800@users.noreply.github.com>
2024-06-12 22:42:20 -0300

    Update script.php
    Adicionado uma melhor visualização na tabela que mostra as informações armazenadas no banco de dados. Tabela esta retornada após clicar no botão retornar dados, isso com os campos preenchidos corretamente é claro

--- login_page/back/script.php
+++ login_page/back/script.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', 'localhost');
 define('PORT', '3306');
-define('DBNAME', 'user_infos');
+define('DBNAME', 'usuarios');
 define('USER', 'lucas');
 define('PASSWORD', '80050308');
 
@@ -10,21 +10,51 @@ try {
 } catch (PDOException $e) {
     echo 'Ocorreu um erro durante a execução:'. $e->getMessage();
 }
-
-$stmt = $dsn -> prepare("INSERT INTO user_infos(nome, cpf, email, nascimento)
+try {
+$stmt = $dsn -> prepare("INSERT INTO clientes(nome_completo, cpf, email, nascimento)
 Values (?,?,?,?)
 ");
 
 $resultset = $stmt -> execute([$_REQUEST['nome_completo'], $_REQUEST['cpf'], $_REQUEST['email'], $_REQUEST['nascimento']]);
 
+
+
+$instrução = "Select nome_completo, cpf, email, nascimento From clientes";
+$resultset = $dsn ->query($instrução);
+
+echo '
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+<table class="table table-dark">
+<thead>
+    <tr>
+        <th scope="col">Nome</th>
+        <th scope="col">CPF</th>
+        <th scope="col">Email</th>
+        <th scope="col">Nascimento</th>
+    </tr>
+</thead>
+<tbody>';
+
 if ($resultset) {
-    echo 'Os dados foram inseridos com sucesso';
+while ($row = $resultset->fetch(PDO::FETCH_ASSOC)) {
+echo '<tr>';
+echo '<td>' . htmlspecialchars($row['nome_completo']) . '</td>';
+echo '<td>' . htmlspecialchars($row['cpf']) . '</td>';
+echo '<td>' . htmlspecialchars($row['email']) . '</td>';
+echo '<td>' . htmlspecialchars($row['nascimento']) . '</td>';
+echo '</tr>';
+}
 } else {
-    echo 'ocorreu um erro';
+echo '<tr><td colspan="4">Nenhum resultado encontrado</td></tr>';
 }
 
-$instrução = "Select nome_completo, cpf, email, nascimento From usuarios";
-$resultset = $dsn ->query($instrução);
+echo '</tbody></table>';
+} catch (PDOException $e) {
+echo 'Ocorreu um erro ao buscar os dados: '. $e->getMessage();
+}
 
 $stmt = null;
 $dsn = null;
+?>
